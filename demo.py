a6f81a1992277602ef786108ab83b3ca636c48c4
lingathotinithin <81841016+lingathotinithin@users.noreply.github.com>
2021-04-06 10:04:26 +0530

    add

---
+++ demo.py
@@ -0,0 +1,7 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=20
+>>> b=30
+>>> a+b
+50
+>>> 
